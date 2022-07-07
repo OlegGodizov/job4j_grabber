a23166f70ff45e6c909b57e676d7252f6a71b636
olegg <oleg.god.2013@mail.ru>
2022-07-07 23:23:30 +0300

    2. Парсинг HTML страницы. [#260358]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,10 +5,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 
 public class HabrCareerParse {
 
@@ -19,21 +19,17 @@ public class HabrCareerParse {
     public static void main(String[] args) throws IOException {
         Connection connection = Jsoup.connect(PAGE_LINK);
         Document document = connection.get();
+        HabrCareerDateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
         Elements rows = document.select(".vacancy-card__inner");
         rows.forEach(row -> {
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
             Element dateElement = row.select(".vacancy-card__date").first().child(0);
-            String date = dateElement.attr("datetime");
-            SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
-            SimpleDateFormat output = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-            try {
-                date = output.format(input.parse(date));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+            String dateAttr = dateElement.attr("datetime");
             String vacancyName = titleElement.text();
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String date = dateTimeParser.parse(dateAttr).format(outputFormatter);
             System.out.printf("%s %s %s%n", vacancyName, link, date);
         });
     }
---
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,12 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HabrCareerDateTimeParser implements DateTimeParser {
+    @Override
+    public LocalDateTime parse(String parse) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+        return LocalDateTime.parse(parse, inputFormatter);
+    }
+}
